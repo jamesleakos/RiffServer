7aa99e5f97efafba031689d329ea884385bad24c
Robert Hu <robert.y.hu.97@gmail.com>
2023-01-19 10:02:24 -0800

    Commented out try catch in get directmessages and added username to get directmessages query

--- server/models/messages.js
+++ server/models/messages.js
@@ -12,19 +12,19 @@ module.exports = {
       });
     },
 
-    getDirectMessages: (user_id, recipient_id) => {
-      return db.query(`SELECT * FROM messages WHERE (user_id=${user_id} AND recipient_id=${recipient_id}) OR (user_id=${recipient_id} AND recipient_id=${user_id})`)
-        .then((result) => {
-          return result.rows;
-        })
-        .catch((err) => {
-          return err;
-        })
-    },
+  getDirectMessages: (user_id, recipient_id) => {
+    return db.query(`SELECT *, (SELECT username FROM users WHERE id = messages.user_id) FROM messages WHERE (user_id=${user_id} AND recipient_id=${recipient_id}) OR (user_id=${recipient_id} AND recipient_id=${user_id})`)
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        return err;
+      })
+  },
 
-    postMessage: (message, server_id, channel_id, user_id, recipient_id) => {
-      const queryString = `INSERT INTO messages (message, server_id, channel_id, user_id, recipient_id, created_at) VALUES ($1, $2, $3, $4, $5, $6)`
-      return db.query(queryString, [message, server_id, channel_id, user_id, recipient_id, Date.now()])
-    },
+  postMessage: (message, server_id, channel_id, user_id, recipient_id) => {
+    const queryString = `INSERT INTO messages (message, server_id, channel_id, user_id, recipient_id, created_at) VALUES ($1, $2, $3, $4, $5, $6)`
+    return db.query(queryString, [message, server_id, channel_id, user_id, recipient_id, Date.now()])
+  },
 
 };
